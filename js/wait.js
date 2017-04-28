40a5e9155b525cb303352aecdbb696f02f829828
lzx <2501172886@qq.com>
2017-04-28 17:08:47 +0800

    js

--- js/wait.js
+++ js/wait.js
@@ -1,28 +1,28 @@
 $(document).ready(function(){
-	$("#wait-pay").click(function(){
-		$(".wait").hide();
-		$(".wait-pay").show();
-		$(".header a").css({"color":"#999","border-bottom":" .02rem solid #fff"});
-		$("#wait-pay").css({"color":"#333","border-bottom":" .02rem solid #DCB469"});
-	});
-	$("#wait-send").click(function(){
-		$(".wait").hide();
-		$(".wait-send").show();
-		$(".header a").css({"color":"#999","border-bottom":" .02rem solid #fff"});
-		$("#wait-send").css({"color":"#333","border-bottom":" .02rem solid #DCB469"});
-	});
-	$("#wait-delivery").click(function(){
-		$(".wait").hide();
-		$(".wait-delivery").show();
-		$(".header a").css({"color":"#999","border-bottom":" .02rem solid #fff"});
-		$("#wait-delivery").css({"color":"#333","border-bottom":" .02rem solid #DCB469"});
-	});
-	$("#orders-finish").click(function(){
-		$(".wait").hide();
-		$(".orders-finish").show();
-		$(".header a").css({"color":"#999","border-bottom":" .02rem solid #fff"});
-		$("#orders-finish").css({"color":"#333","border-bottom":" .02rem solid #DCB469"});
-	});
+	$(".header>li").on("click",function (){
+		var index = $(this).index();
+		show(index);
+	})
 	
+	var id =window.location.href.split("=")[1];
+	if(id){
+		switch (id){
+			case "0": show(id);
+			break;
+			case "1": show(id);
+			break;
+			case "2": show(id);
+			break;
+			case "3": show(id);
+			default:
+				break;
+		}
+	}
+	
+//	订单操作
+	function show (id) {
+		$(".header>li").eq(id).find("a").addClass("active").parent().siblings().find("a").removeClass("active");
+		$(".wait").eq(id).show().siblings(".wait").hide();
+	}
 })
 
